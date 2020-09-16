3d9a1a48c664e9594adf9e9ff8fd1fd66c800a60
anndonut <69615415+anndonut@users.noreply.github.com>
2020-09-16 20:41:15 +0900

    新規登録画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  devise_for :users
   root to: "home#index"
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'home/index'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  # resources: users, only:[:new, :create]
+  
+  
 end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
