d36b3d86e390775f6a4fcafe6151cfcb8871be53
anndonut <69615415+anndonut@users.noreply.github.com>
2020-09-16 12:53:07 +0900

    トップページの配置

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :anthenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
-  root to: "users#index"
-  resources :users, only: [:new]
+  root to: "home#index"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
