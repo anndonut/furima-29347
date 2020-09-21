6e8f9b69e64d2af9af6567654cecd0d101e0ff0e
anndonut <69615415+anndonut@users.noreply.github.com>
2020-09-21 14:22:11 +0900

    バリデーションの設定
    ユーザー情報
    本人情報の確認

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname,:first_name,:family_name,:first_name_kana, :family_name_kana,:birth_day ,presence: true
- 
+  # validates :password,presence:true,length:{minimum:6}
+  # validates :password, confirmation: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :family_name
+  end
+
+  with_options presence: true, format: { with: /\A[ｧ-ﾝﾞﾟ]+\z/, message: '全角文字(カナ)を使用してください' } do
+    validates :first_name_kana
+    validates :family_name_kana
+  end
+
+  with_options presence: true, format: { with:/\A[0-9]+\z/, message: '生年月日を入力してください' } do
+    validates :birth_day
+  end
 end
