cb65e6665e96a42dd9376dbb3079c7f4432e5e4e
anndonut <69615415+anndonut@users.noreply.github.com>
2020-09-22 12:30:48 +0900

    テストコードの記述
    モデル

---
+++ spec/factories/users.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+
+  factory :user do
+    first_name            {“kkk”}
+    first_name_kana       {“カタカナ”}
+    last_name             {“kkk”}
+    last_name_kana        {“カタカナ”}
+    nickname              {Faker::Name.initials(number: 2)}
+    birth_year            {2020}
+    birth_month           {1}
+    birth_day             {1}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+  end
+end
