b677362ad93a7a132a334580e57f697c4a1bed99
anndonut <69615415+anndonut@users.noreply.github.com>
2020-09-24 11:33:32 +0900

    一覧表示機能の実装（カテゴリーと商品状態）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
 
   def index
+    @items = Item.order("created_at DESC")
   end
 
   def new
@@ -8,6 +9,24 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end 
   end
-  
+
+  private 
+    
+  def item_params
+    params.require(:item).permit(
+      :category_id,
+      :condition_id,
+      :shipping_expense_id,
+      :ship_from_area_id,
+      :processing_time_id
+    )
+  end
 end
+
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,12 +2,24 @@ class Item < ApplicationRecord
   belongs_to :user, class_name: "User"
   # has_one :transaction, class_name: "Transaction"
 
-  validates :image ,:name,:introduction, :category_id, :shipping_expense_id, :ship_from_area_id, :processing_time_id, :selling_price,  presence: true
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to_active_hash :category
+    belongs_to_active_hash :condition
 
-  validates :selling_price, inclusion: {in: 300..9999999}
+    # :shipping_expense,:ship_from_area,:processing_time
 
-  with_options presence: true, format: { with: /\A[0-9]+\z/, message: '半角数字を使用してください' } do
-    validates :selling_price
+    #選択が「--」のままになっていないか
+    with_options numericality: { other_than: 1 } do
+      validates :category_id,:condition_id,:shipping_expense_id,:ship_from_area_id,:processing_time_id
   end
+
+
+  # validates :image ,:name,:introduction, :category_id, :shipping_expense_id, :ship_from_area_id, :processing_time_id, :selling_price,  presence: true
+
+  # validates :selling_price, inclusion: {in: 300..9999999}
+
+  # with_options presence: true, format: { with: /\A[0-9]+\z/, message: '半角数字を使用してください' } do
+  #   validates :selling_price
+  # end
 
 end
