c07ccb680ff1c886e6b80a5bae78208974177dfe
anndonut <69615415+anndonut@users.noreply.github.com>
2020-09-24 21:39:28 +0900

    テストコードの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,9 +9,10 @@ class Item < ApplicationRecord
     self.image.attached?
   end
 
+  validates :name,:introduction, :category_id, :shipping_expense_id, :ship_from_area_id, :processing_time_id,   presence: true
 
     #選択が「--」のままになっていないか
-    with_options numericality: { other_than: 1 } do
+    with_options numericality: { other_than: 0 } do
       validates :category_id,:condition_id,:shipping_expense_id,:ship_from_area_id,:processing_time_id
   end
 
@@ -23,12 +24,26 @@ class Item < ApplicationRecord
   belongs_to_active_hash :ship_from_area
   belongs_to_active_hash :processing_time
 
-  # validates :image ,:name,:introduction, :category_id, :shipping_expense_id, :ship_from_area_id, :processing_time_id, :selling_price,  presence: true
+
+  # with_options presence: true, format: { message: 'Name can't be blank' } do
+  #   validates :name
+  # end
+
+
+ 
 
   # validates :selling_price, inclusion: {in: 300..9999999}
 
-  # with_options presence: true, format: { with: /\A[0-9]+\z/, message: '半角数字を使用してください' } do
+  validates :selling_price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+
+
+  # with_options presence: true, :numericality { with: /\A[0-9]+\z/, message: 'Selling price 半角数字を使用してください' } do
   #   validates :selling_price
   # end
+
+  
+  validates :selling_price, numericality: { only_integer: true } 
+  
+
 
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,101 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.build(:item, user_id: @user.id)
+
+  end
+
+  
+    # name,introduction,category,condition,shipping_expense,ship_from_area,prossing_time,selling_priceがあれば出品できること
+    it 'name,introduction,category,condition,shipping_expense,ship_from_area,processing_time,selling_priceがあれば出品できる' do
+      expect(@item).to be_valid
+    end
+    
+
+
+    # 画像が空だと出品できないこと
+    it '画像が空だと出品できない'do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include ("Image can't be blank")
+    end
+
+    # 商品名が空だと出品できないこと
+    it '商品名が空だと出品できない'do
+      @item.name = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include ("Name can't be blank")
+    end
+
+
+
+    # 商品の説明が空だと出品できないこと
+    it '商品の説明が空だと出品できない'do
+      @item.introduction = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include ("Introduction can't be blank")
+    end
+
+
+    # カテゴリーの情報が空だと出品できないこと
+    it 'カテゴリーが空だと出品できない'do
+      @item.category = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include ("Category can't be blank")
+    end
+
+
+    # 商品の状態についての情報が空だと出品できないこと
+    it '商品の状態が空だと出品できない'do
+      @item.condition = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include ("Condition is not a number")
+    end
+
+
+    # 配送料の負担についての情報が空だと出品できないこと
+    it '配送料の負担が空だと出品できない'do
+      @item.shipping_expense = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include ("Shipping expense can't be blank")
+    end
+
+    # 発送元の地域についての情報が空だと出品できないこと
+    it '配送先の地域が空だと出品できない'do
+      @item.ship_from_area = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include ("Ship from area can't be blank")
+    end
+
+    # 発送までの日数についての情報が空だと出品できないこと
+    it '配送先の地域が空だと出品できない'do
+      @item.processing_time = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include ("Processing time can't be blank")
+    end
+
+
+    # 価格についての情報が空だと出品できないこと
+    it '価格が空だと出品できない'do
+      @item.selling_price = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include ("Selling price is not a number")
+    end
+
+    # 価格の範囲が、¥300~¥9,999,999の間でなければ出品できないこと
+    it '価格の範囲が、¥300~¥9,999,999の間でなければ出品できない'do
+      @item.selling_price  = 200
+      @item.valid?
+      expect(@item.errors.full_messages).to include ("Selling price must be greater than or equal to 300")
+   end
+
+    # 販売価格は半角数字のみ保存可能であること
+    it '販売価格は半角数字でなければ出品できない'do
+    @item.selling_price  = "あいうえお"
+    @item.valid?
+    expect(@item.errors.full_messages).to include ("Selling price is not a number")
+    end
+
 end
