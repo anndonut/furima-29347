7376e5c5a553d083edcfcc075d3afe2c7ccd98e8
anndonut <69615415+anndonut@users.noreply.github.com>
2020-09-27 11:32:34 +0900

    商品情報編集の更新機能の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,7 +7,22 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    if user_signed_in? && current_user.id == @item.user_id
+      edit_item_path
+    else
+      render :new
+    end
   end
+
+  def update
+    item = Item.find(params[:id])
+    if user_signed_in? && current_user.id == item.user_id
+      item.update(item_params)
+      redirect_to root_path
+    else
+      render :new
+    end
+  end 
 
   def destroy
   end
