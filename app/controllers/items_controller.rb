0fed57e1d4a3e8b4fdd512ec924c3f127a73c8aa
anndonut <69615415+anndonut@users.noreply.github.com>
2020-10-01 20:49:29 +0900

    updated

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -32,13 +32,15 @@ class ItemsController < ApplicationController
   end 
 
   def destroy
-    @item = Item.find(params[:id])
     if user_signed_in? && current_user.id == @item.user_id
-      @item.destroy
-      redirect_to root_path
-    else
-      render :edit
-    end
+      if @item.destroy
+       redirect_to root_path
+      else
+       render :show
+      end
+     else
+       render :show
+     end
   end
 
 
