4e3afffd57f47a5f4b8e1ddbe7e8c4295ab45f07
anndonut <69615415+anndonut@users.noreply.github.com>
2020-10-05 21:59:04 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -10,5 +11,13 @@ class ApplicationController < ActionController::Base
 
   def show
     @nickname = current_user.nickname
+  end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
+    end
   end
 end
